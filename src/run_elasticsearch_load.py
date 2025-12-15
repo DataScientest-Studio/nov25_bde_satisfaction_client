7a3fe91c36fc588ebd86d149d31584a66754fc1d
ibbouM <ibbou96@gmail.com>
2025-12-15 12:45:19 +0100

    feat: supprimer les anciens fichiers

--- src/run_elasticsearch_load.py
+++
@@ -1,5 +0,0 @@
-from es_kibana.jsonl_loader import load_last_jsonl
-from es_kibana.elasticsearch_bulk_loader import load_reviews_to_elasticsearch_bulk
-
-documents = load_last_jsonl("es_kibana/data")
-load_reviews_to_elasticsearch_bulk(documents=documents)
