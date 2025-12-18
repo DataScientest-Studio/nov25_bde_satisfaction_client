d8f27d3c1f80a773fb08046234056aaa6a15ecb4
ibbouM <ibbou96@gmail.com>
2025-12-18 14:59:28 +0100

    feat: créer le fichier predict avec une fonction qui permet d'appliquer le modèle nlp sur des données en entrée

---
+++ src/machine_learning/predict.py
@@ -0,0 +1,57 @@
+# contient le tokenizer et le modèle de sentiment
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# sert à faire les calculs du modèle
+import torch
+
+
+model_name = "tabularisai/multilingual-sentiment-analysis"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+sentiment_map = {
+    0: "Très négatif",
+    1: "Négatif",
+    2: "Neutre",
+    3: "Positif",
+    4: "Très positif"
+}
+
+
+def predict_sentiment(text):
+    """
+    Prédit le sentiment d'un texte à l'aide d'un modèle de NLP (Natural Language Processing => 
+    Traitement du langage naturel).
+
+    :param text: Texte à analyser (avis utilisateur)
+
+    :return: Dictionnaire contenant le sentiment prédit et le score de confiance
+    """
+    # Tokenisation du texte
+    inputs = tokenizer(
+        text,
+        return_tensors="pt",
+        truncation=True,
+        padding=True,
+        max_length=512
+    )
+
+    # Passage du texte tokenisé dans le modèle
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    probabilities = torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+    # _ permet d'ignorer le score, seule la classe prediction est utilisé
+    _, prediction = torch.max(probabilities, dim=-1)
+
+    return {
+        "text": text,
+        "sentiment": sentiment_map[prediction.item()],
+    }
+
+
+if __name__ == "__main__":
+    text = input("Entre un avis : ")
+    result = predict_sentiment(text)
+    print(f"Avis : {text}")
+    print(f"Sentiment : {result['sentiment']}")
