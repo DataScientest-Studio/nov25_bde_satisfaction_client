f106b25b845a9304a56925fd00e596869a3d7ced
ibbouM <ibbou96@gmail.com>
2025-12-23 11:36:36 +0100

    feat: ajout d'un message d'erreur quand le texte est vide ou invalide dans fastapi

--- src/api/routes/predict.py
+++ src/api/routes/predict.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.api.schemas import PredictRequest, PredictResponse
 from src.machine_learning.predict import predict_sentiment
 
@@ -13,9 +13,11 @@ router = APIRouter()
     summary="Prédire le sentiment d'un avis client",
     description="Cette endpoint prend en entrée un texte d'avis client et retourne le sentiment prédit (NEGATIF, NEUTRE, POSITIF).",
     response_description="Le sentiment prédit de l'avis client",
-    response_model=PredictResponse
+    response_model=PredictResponse,
 )
 # ajout de la fonction de gestion de la route
 def predict(request: PredictRequest) -> PredictResponse:
-    return predict_sentiment(request.text)
-
+    try:
+        return predict_sentiment(request.text)
+    except ValueError as error:
+        raise HTTPException(status_code=400, detail=str(error))
